40d9e7fc7d767cb40686f81a54aa118bd8e48912
Melozzo <carmecamma1995@gmail.com>
2023-10-19 18:29:33 +0200

    [edit]: banner section

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,28 +6,37 @@
 
 <template>
   <header>
-    <div class="newUpdate container d-flex justify-content-between">
-      <div class="d-flex align-items-center">
-        <div class="profileButton">
-          <span class="buttonUpdates">NEWS UPDATES</span>
-          <img class="profileImage" src="/image/ProfiloImage.jpg" alt="profilo">
+    <div class="newsUpdate">
+      <div class="containerHead container d-flex justify-content-between">
+        <div class="d-flex align-items-center">
+          <div class="profileButton">
+            <span class="buttonUpdates">NEWS UPDATES</span>
+            <img class="profileImage" src="/image/ProfiloImage.jpg" alt="profilo">
+          </div>
+          <span> 05:52 the_</span>
         </div>
-        <span> 05:52 the_</span>
-      </div>
-
-      <div class="socialHeader d-flex align-items-center">
-        <div class="iconeArrow">
-          &leftarrow; &rightarrow;
-        </div>
-        <div class="iconeSocial">
-          icone
+  
+        <div class="socialHeader d-flex align-items-center">
+          <div class="iconeArrow">
+            &leftarrow; &rightarrow;
+          </div>
+          <div class="iconeSocial">
+            icone
+          </div>
         </div>
       </div>
     </div>
 
-    <section class="banner">
-      <img src="/public/image/anime-whitelogo-300x89.png" alt="">
+    <section class="banner container d-flex justify-content-between align-items-center ">
+      <div class="containerBanner">
+        <img id="logo" src="/public/image/anime-logo-300x89.webp" alt="logo-red">
+      </div>
+
+      <div >
+        <img class="bannerImage" src="/public/image/header-banner.webp" alt="banner">
+      </div>
     </section>
+
   </header>
 </template>
 
