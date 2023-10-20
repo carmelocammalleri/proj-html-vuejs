27f722d2d78aa6ad67b00727b6c23a543050156c
Melozzo <carmecamma1995@gmail.com>
2023-10-20 10:15:07 +0200

    [add]: comments in main (TTD)

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,7 +6,33 @@
 
 <template>
   <main>
-    ciao main
+    <!-- slider -->
+    
+    <!-- slider -->
+
+    <!-- photo gallery -->
+    
+    <!-- photo gallery -->
+
+    <!-- lifestyle and stories -->
+
+    <!-- lifestyle and stories -->
+
+    <!-- banner -->
+    
+    <!-- banner -->
+
+    <!-- feautured post -->
+    
+    <!-- feautured post -->
+
+    <!-- youtube -->
+    
+    <!-- youtube -->
+
+    <!-- elementi e foto -->
+    
+    <!-- elementi e foto -->
   </main>
 </template>
 
