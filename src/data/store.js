28c29ae807ad636d4fb36734d52de9d2451dca6b
Melozzo <carmecamma1995@gmail.com>
2023-10-20 11:47:39 +0200

    [create]: array to use it in cicle

---
+++ src/data/store.js
@@ -0,0 +1,26 @@
+export const cardsList= [
+    {id: 123,
+    src: '/healthy-foods.webp',
+    title: 'The best healty food',
+    date: 'december 2022',
+    btn: 'food'
+    },
+    {id: 153,
+    src: '/winter.webp',
+    title: 'The best healty food',
+    date: 'december 2022',
+    btn: 'food'
+    },
+    {id: 156,
+    src: '/photographers-mistakes.webp',
+    title: 'The best healty food',
+    date: 'december 2022',
+    btn: 'food'
+    },
+    {id: 158,
+    src: '/rice-ball.webp',
+    title: 'The best healty food',
+    date: 'december 2022',
+    btn: 'food'
+    }
+  ]
