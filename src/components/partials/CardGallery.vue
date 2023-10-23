055c219276cf5f8340a40b03f3713e207755f0f6
Melozzo <carmecamma1995@gmail.com>
2023-10-23 17:15:39 +0200

    [fix]: card gallery error

--- src/components/partials/CardGallery.vue
+++ src/components/partials/CardGallery.vue
@@ -13,23 +13,50 @@
   <section class="photoGallery py-4">
     <div class="container d-flex">
       <div class="left">
-        <div class="card due mb-2">ciao</div>
-        <div class="card due">ciao</div>
+        <div class="card double mb-2">
+          <img src="/public/image/photographers-mistakes.webp" alt="">
+          <div class="btnInfo">
+            <span class="mx-1">Travel</span>
+            <span>Lifestyle</span>
+          </div>
+        </div>
+        <div class="card double">
+          <img src="/public/image/photographers-mistakes.webp" alt="">
+          <div class="btnInfo">
+            <span class="mx-1">Travel</span>
+            <span>Lifestyle</span>
+          </div>
+        </div>
       </div>
       <div class="middle">
-        <div class="card uno">ciao</div>
+        <div class="card single">
+          <img src="/public/image/best-places-300x200.webp" alt="">
+          <div class="btnInfo">
+            <span class="mx-1">Travel</span>
+            <span class="mx-1">Lifestyle</span>
+            <span class="mx-1">Food</span>
+          </div>
+        </div>
       </div>
       <div class="left">
-        <div class="card due mb-2">ciao</div>
-        <div class="card due">ciao</div>
+        <div class="card double mb-2">
+          <img src="/public/image/photographers-mistakes.webp" alt="">
+          <div class="btnInfo">
+            <span class="mx-1">Travel</span>
+            <span>Lifestyle</span>
+          </div>
+        </div>
+        <div class="card double">
+          <img src="/public/image/photographers-mistakes.webp" alt="">
+          <div class="btnInfo">
+            <span class="mx-1">Travel</span>
+            <span>Lifestyle</span>
+          </div>
+        </div>
       </div>
 
     </div>
   </section>
-
-
-
-
 
 </template>
 
